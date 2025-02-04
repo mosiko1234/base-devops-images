2c7ed85759248cc3e8c909161afa63d7a4b22af5
mosiko1234 <41381915+mosiko1234@users.noreply.github.com>
2025-02-04 17:18:48 +0200

    ננענ

--- .github/scripts/generate_matrix.py
+++ .github/scripts/generate_matrix.py
@@ -8,18 +8,22 @@ with open("languages.yaml") as f:
 with open("addons.yaml") as f:
     addons = yaml.safe_load(f)["addons"]
 
+# שמירת תלויות בסיסיות (כגון curl, git, wget)
+base_packages = " ".join(addons.get("base_packages", []))
+
 matrix = []
 
 for lang, versions in languages.items():
     for version in versions:
         base_image = f"openjdk:{version}-jdk-bullseye" if lang == "java" else f"{lang}:{version}"
-        packages = " ".join(addons.get(lang, []))
+        lang_packages = " ".join(addons.get(lang, []))
 
         matrix.append({
             "language": lang,
             "version": version,
             "base_image": base_image,
-            "packages": packages
+            "base_packages": base_packages,  # חבילות מערכת הפעלה
+            "packages": lang_packages        # חבילות ספציפיות לשפה
         })
 
 # כתיבת המטריצה כקובץ JSON קריא
